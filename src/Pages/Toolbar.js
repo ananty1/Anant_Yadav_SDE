20cdcb7f521c85186f3f9be119f22f3894f9f43f
anant <ananty@iitbhilai.ac.in>
2024-08-28 01:54:11 +0530

    Added the user friendly closing sidebar

--- src/Pages/Toolbar.js
+++ src/Pages/Toolbar.js
@@ -44,7 +44,7 @@ const Toolbar = () => {
             <div onClick={handleNav} className='sm:hidden z-10'>
                 <FaBars size={20} className='mr=4 cursor-pointer' />
             </div>
-            <div className={nav ? 'overflow-y-hidden md:hidden  duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col' : 'absolute top-0 h-screen left-[-100%] ease-in duration-500 '}>
+            <div className={nav ? 'overflow-y-hidden md:hidden  duration-300 absolute text-gray-300 left-0 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col' : 'absolute top-0 h-screen left-[-100%] ease-in duration-500 '} onClick={handleNav}>
                 <ul className='h-full w-full text-center pt-12'>
                     <li className='text-2xl py-8'>
                         <Link to={"/"}> Home</Link>
